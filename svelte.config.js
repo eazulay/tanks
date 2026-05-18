3510f1cd520498461ef6e1d62fe2559375d6b6a5
Eyal Azulay <eyal.git@tiyal.com>
2026-05-18 08:54:24 +0200

    Added paths to svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/tanks'
+		}
 	}
 };
 
